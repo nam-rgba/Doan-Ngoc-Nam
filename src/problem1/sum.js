2055f5dab0d11d49ac2b755ae67f7cb1ade06cd1
Nam <doanngocnam3002@gmail.com>
2024-05-15 15:01:26 +0700

    update(solution): problem 1 finish

---
+++ src/problem1/sum.js
@@ -0,0 +1,28 @@
+var sum_to_n_a = function(n) {
+    // your code here
+    if (n === 1) return 1;
+    var sum = 0;
+    for (var i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+    // O(n)
+};
+
+var sum_to_n_b = function(n) {
+    // your code here
+    return (n * (n + 1)) / 2
+    // O(1)
+};
+
+var sum_to_n_c = function(n) {
+    // your code here
+    const arr = Array.from({ length: n }, (_, i) => i + 1);
+    return arr.reduce((a, b) => a + b);
+    // O(n)
+};
+
+console.log(sum_to_n_c(6)); 
+console.log(sum_to_n_b(7)); 
+console.log(sum_to_n_a(5)); 
+
